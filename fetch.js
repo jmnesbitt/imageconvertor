302c9e7b44ea475085a692e8a70177612e1fd752
Jen Nesbitt <jmnesbitt@users.noreply.github.com>
2023-02-02 22:37:08 +0000

    t

---
+++ fetch.js
@@ -0,0 +1,13 @@
+// Using fetch
+async function downloadImage(imageSrc) {
+    const image = await fetch(imageSrc)
+    const imageBlog = await image.blob()
+    const imageURL = URL.createObjectURL(imageBlog)
+  
+    const link = document.createElement('a')
+    link.href = imageURL
+    link.download = 'preview.jpg'
+    document.body.appendChild(link)
+    link.click()
+    document.body.removeChild(link)
+  }
