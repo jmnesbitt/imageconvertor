6309cf7ed786c7232cbdaaf036544d19c5073ce3
Jen Nesbitt <jmnesbitt@users.noreply.github.com>
2023-02-02 15:20:36 +0000

    test

---
+++ index.js
@@ -0,0 +1,20 @@
+const fs = require('fs')
+
+const https = require('https')
+
+function saveImageToDisk(url,path) {
+
+    var fullUrl = url
+
+    var localPath = fs.createWriteStream(path)
+
+    var request = https.get(fullUrl, function (response){
+        console.log(response)
+
+        response.pipe(localPath)
+    })
+
+
+}
+
+saveImageToDisk("https://relay.viewcam.co.uk/sidmouthjacobs/preview.jpg" "/workspaces/imageconvertor/node_modules/uploads" + Date.now() + ".jpg")
