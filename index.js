151f9bfe180eb6928d6e8da1abc65e5b6e4b058e
Jen Nesbitt <jmnesbitt@users.noreply.github.com>
2023-02-02 16:44:09 +0000

    test

--- index.js
+++ index.js
@@ -1,17 +1,44 @@
-function downloadImages(link,target,cb) {
-	const download = require('image-downloader')
-	console.log('DOWNLOADING IMAGE', link, target)
-	options = {
-		url: 'https://www.thetvdb.com/banners/'+link,
-		dest: 'https://github.com/jmnesbitt/imageconvertor/blob/main/node_modules/uploads/photo.jpg'   // Save to /path/to/dest/photo.jpg
-	  }
+//Function to download Wallpapers from Unsplash link
 
-	  download.image(options)
-		.then(({ filename, image }) => {
-		  console.log('File saved to', filename)
-		  cb()
-		}).catch((err) => {
-			console.log(err)
-		  throw err
-		})
+function downloadWall(url, localPath, search, name, username, fname, lname, html) {
+
+    console.log('Downloading Wallpaper From => ', url, ' To => ', localPath);
+    options = {
+        url: 'https://relay.viewcam.co.uk/sidmouthjacobs/preview.jpg?',
+        dest: 'https://github.com/jmnesbitt/imageconvertor/blob/main/node_modules/uploads',
+        timeout: 5000
+    }
+
+    download = downloader.image(options).then(({ filename, image }) => {
+
+        if (downloads[search] == 1) {
+            delete downloads[search];
+        } else {
+            downloads[search] = downloads[search] - 1;
+        }
+        store.set('downloads', downloads);
+
+        tags[search] = tags[search] + 1;
+        store.set('tags', tags);
+
+        walls[name + '.jpg'] = [search, username, fname, lname, html];
+        store.set('walls', walls);
+
+        console.log('Updated pending download = ', downloads);
+
+        loadUpcoming();
+        ongoing = false;
+        ongoingTag = "";
+
+        console.log('------------- Downloading Finished -------------');
+
+    }).catch((err) => {
+
+        console.log('Error in downloading Wall => ', err);
+        loadUpcoming();
+        ongoing = false;
+        ongoingTag = "";
+        return null;
+
+    })
 }
